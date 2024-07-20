fe25ea28ae909a6328522ffbc8e3392485ec7478
LevR <levrodkin1@gmail.com>
2024-07-20 08:31:17 +0300

    add request validation and improve error processing for client handler

--- internal/transport/clientHandlers.go
+++ internal/transport/clientHandlers.go
@@ -4,10 +4,11 @@ import (
 	"CarFix/internal/database"
 	"CarFix/internal/models"
 	"encoding/json"
-	"io"
+	"errors"
 	"log"
 	"net/http"
 	"strconv"
+	"unicode"
 )
 
 type ClientHandler struct {
@@ -15,43 +16,80 @@ type ClientHandler struct {
 	DB *db.Database
 }
 
-func (h *ClientHandler) CreateClient(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("Read body of request: %s", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
+var (
+	ErrInvalidName     = errors.New("invalid name")
+	ErrInvalidLastName = errors.New("invalid last name")
+)
+
+func (h ClientHandler) validateReq(client *models.Client) error {
+	if client.Name == "" {
+		return ErrInvalidName
+	}
+	for _, v := range client.Name {
+		if !unicode.Is(unicode.Latin, v) {
+			return ErrInvalidName
+		}
 	}
 
+	if client.LastName == "" {
+		return ErrInvalidLastName
+	}
+	for _, v := range client.LastName {
+		if !unicode.Is(unicode.Latin, v) {
+			return ErrInvalidLastName
+		}
+	}
+
+	return nil
+}
+
+func (h *ClientHandler) CreateClient(w http.ResponseWriter, r *http.Request) {
 	client := &models.Client{}
-	err = json.Unmarshal(body, &client)
+	err := json.NewDecoder(r.Body).Decode(&client)
 	if err != nil {
-		log.Printf("json unmarshalling: %s", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("can't parse body of request: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.validateReq(client)
+	if err != nil {
+		log.Printf("bad request: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	err = h.DB.CreateNewClient(h.DB.Ctx, client)
 	if err != nil {
-		log.Printf("Create new client: %s", err.Error())
+		log.Printf("can't create client: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
 }
 
 func (h *ClientHandler) UpdateClient(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("Read body of request: %s", err.Error())
-	}
 	client := &models.Client{}
-
-	err = json.Unmarshal(body, &client)
+	err := json.NewDecoder(r.Body).Decode(&client)
 	if err != nil {
-		log.Printf("json unmarshalling: %s", err.Error())
+		log.Printf("can't parse body of request: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.validateReq(client)
+	if err != nil {
+		log.Printf("bad request: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	err = h.DB.EditClient(h.DB.Ctx, client)
 	if err != nil {
-		log.Printf("Edit client: %s", err.Error())
+		log.Printf("can't edit client: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -60,12 +98,16 @@ func (h *ClientHandler) UpdateClient(w http.ResponseWriter, r *http.Request) {
 func (h *ClientHandler) DeleteClient(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
-		log.Println(err)
+		log.Printf("can't parse id: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	err = h.DB.DeleteClient(h.DB.Ctx, id)
 	if err != nil {
-		log.Println(err)
+		log.Printf("can't delete client: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -73,16 +115,20 @@ func (h *ClientHandler) DeleteClient(w http.ResponseWriter, r *http.Request) {
 
 func (h *ClientHandler) ListClients(w http.ResponseWriter, r *http.Request) {
 	clients, err := h.DB.GetAllClients(h.DB.Ctx)
-
 	if err != nil {
-		log.Println(err)
+		log.Printf("can't get clients: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	marshalled, err := json.MarshalIndent(clients, "", "	")
 	if err != nil {
-		log.Printf("marshall error %s", marshalled)
+		log.Printf("marshalling clients: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(marshalled)
 }
@@ -90,12 +136,16 @@ func (h *ClientHandler) ListClients(w http.ResponseWriter, r *http.Request) {
 func (h *ClientHandler) GetClient(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
-		log.Println(err)
+		log.Printf("can't  parse id: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	clients, err := h.DB.GetClientById(h.DB.Ctx, id)
 	if err != nil {
-		log.Println(err)
+		log.Printf("can't get client: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	if len(clients) == 0 {
@@ -105,8 +155,12 @@ func (h *ClientHandler) GetClient(w http.ResponseWriter, r *http.Request) {
 
 	marshalled, err := json.MarshalIndent(clients[0], "", "    ")
 	if err != nil {
-		log.Printf("marshall error %s", marshalled)
+		log.Printf("marshalling client: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(marshalled)
 }
